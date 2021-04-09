2ef7420a41101faf0fb79cf3dd4d4640c1aed60e
nathannishi <nnhusky7@uw.edu>
2021-04-09 14:01:20 -0700

    milestone1.cpp main execute test

--- milestone1.cpp
+++ milestone1.cpp
@@ -12,4 +12,36 @@
 using namespace std;
 using namespace hsql;
 
+string execute(const SQLStatement *stmt) {
+    switch (stmt->type()) {
+        case kStmtSelect:
+            return stmt;
+        case kStmtInsert:
+            return stmt;
+        case kStmtCreate:
+            return stmt;
+        default:
+            return "Not implemented";
+    }
+}
+
+int main(int argc, char **argv) {
+
+    char *envDir = argv[1];
+    DbEnv env(0U);
+    env.set_message_stream(&cout);
+    env.set_error_stream(&cerr);
+    while (true) {
+        cout << "SQL> ";
+        string stmt;
+        getline(cin, stmt);
+        if (stmt == "quit") break;
+        SQLParserResult *parseTree = SQLParser::parseSQLString(stmt);
+        for (uint i = 0; i < parseTree->size(); ++i) {
+            cout << execute(parseTree->getStatement(i)) << endl;
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
 
